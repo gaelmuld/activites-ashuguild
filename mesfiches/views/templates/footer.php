2eb106d740f2be1aac78244cdb5e34e97df9e415
Mulders Gael <ga3lmulders@gmail.com>
2018-01-07 23:43:02 +0100

    fix bug creation activité

--- mesfiches/views/templates/footer.php
+++ mesfiches/views/templates/footer.php
@@ -1,10 +1,10 @@
-<?php if((@$_SESSION['rang']>3 || @$activite['createur'] === @$_SESSION['id'] && @$_SESSION['rang']>2) ){ ?>
+<?php if(@$_SESSION['rang']>2){ ?>
 
 <script src='<?= js_url("gestion") ?>'></script>
 
-<?php } if(@$vue =='activity'){ ?>
+<?php  if(@$vue =='activity'){ ?>
 <script src='<?= js_url("participation") ?>'></script>
-<?php } ?>
+<?php }} ?>
 </body>
 
 </html>
